e7fea5310686938a5dc0cc74b6afd2d6acc257a8
leungnamson1023 <leungnamson@gmail.com>
2018-01-22 11:32:11 -0800

    added a comment line to define what this is

--- src/Bee.js
+++ src/Bee.js
@@ -2,7 +2,7 @@ var Bee = function() {
   Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
-  this.food = this.food; // Grub food property;
+  this.food = this.food; // right hand this.food = Grub food property;
   this.job = 'keep on growing';
 };
 
