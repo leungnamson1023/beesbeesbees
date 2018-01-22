e4042daa1997c7f4ec43c73b5a7e96cf00bdeaae
leungnamson1023 <leungnamson@gmail.com>
2018-01-22 11:43:17 -0800

    Created a SuperClass for HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
 var HoneyMakerBee = function() {
+  Bee.call(this); // inherit properties from Bee
+  
+  this.age = 10;
+  this.job = 'make honey';
+  this.yellow = 'pink'
+  this.honeyPot = 0;
+  
 };
 
+
+HoneyMakerBee.prototype = Object.create(Grub); // have methods/property from Grub
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.eat = function(){};
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++; // should only point to this HoneyMakers Honey Pot
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--; // should only point to this HoneyMakers Honey Pot
+}
