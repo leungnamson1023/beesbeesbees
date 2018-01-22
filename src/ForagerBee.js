4d837626faff4447aae5b54f26d72fb37095a77f
leungnamson1023 <leungnamson@gmail.com>
2018-01-22 11:48:39 -0800

    created subClass for ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,28 @@
 var ForagerBee = function() {
+  Bee.call(this); 
+  this.age = 10;
+  this.job = 'find pollen'
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+
+
+
+
+
+
+
+
+
+
+
+ForagerBee.prototype = Object.create(Grub);
+
+ForagerBee.prototype.eat = function(){};
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(thing) {
+  this.treasureChest.push(thing); // only delegates to this ForageBee treasure chest
+}
