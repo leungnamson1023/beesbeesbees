63010cd05c3e4331a1ac115e5c598f1a7b113fcb
leungnamson1023 <leungnamson@gmail.com>
2018-01-22 11:53:35 -0800

    created subClass for RetiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,32 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this); 
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
 };
 
+
+
+
+
+
+
+
+
+
+RetiredForagerBee.prototype = Object.create(Grub);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead"
+};
+
+RetiredForagerBee.prototype.eat = function(){};
+
+RetiredForagerBee.prototype.gamble = function(win) {
+  this.treasureChest.push(win);
+};
